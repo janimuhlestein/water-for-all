7929348f94cf7e42f15980ed4e43b644fbb50114
Jani Muhlestein <janimuhlestein@gmail.com>
2023-01-14 19:13:35 -0700

    added .gitignore

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,2 +1,6 @@
-const router = require('sequelize').Router();
+const router = require('express').Router();
 const userRoutes = require('./user-routes');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
